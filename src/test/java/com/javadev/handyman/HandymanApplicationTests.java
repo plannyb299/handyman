2ef9035cf175677d75a49af7baa6f6cf4fc41b9c
plannyb299 <plannyb29@gmail.com>
2023-02-28 23:35:37 +0200

    first commit

---
+++ src/test/java/com/javadev/handyman/HandymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.handyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
